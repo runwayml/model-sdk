02a395af574b9bf28a0a53164ca19e4c3e141f7f
Alejandro <matamala@material.cl>
2019-04-17 12:03:49 -0400

    sidenav search rtd

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -4,3 +4,11 @@
 if (location.protocol != 'https:') {
  location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 }
+
+// Hide RTD search.
+
+window.document.onload = function(){ 
+  var s = document.getElementsByClassName("injected")
+  s[0].children[3].style.display = 'none';
+}
+
