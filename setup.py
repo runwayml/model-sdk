dc815930b6a4cd0c9799917b25c16092ca14ec4b
Anastasis Germanidis <agermanidis@gmail.com>
2019-04-19 18:22:16 -0400

    bump version and fix pypi description (#37)

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
     scripts=[],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     install_requires=requirements,
-    license=license,
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
